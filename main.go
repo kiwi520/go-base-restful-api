cdb73ba8b1ec4d9e1c545e9decf8a179293784f1
hushunhui <hushunhui@antlinker.com>
2018-04-20 18:09:41 +0800

    添加token

--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import (
 	"github.com/gorilla/mux"
 	"encoding/json"
 	"log"
+	"github.com/dgrijalva/jwt-go/request"
+	"github.com/dgrijalva/jwt-go"
+	"fmt"
+	"strings"
+	"time"
+	"github.com/codegangsta/negroni"
 )
+
+const SecretKey  = "welcome to wangshubo's blog"
 
 //简单声明俩结构
 type Person struct {
@@ -20,6 +28,27 @@ type Address struct {
 	City string `json:",omitempty"`
 	County string `json:"county,omitempty"`
 }
+
+type UserCredentials struct {
+	UserName string `json:"username"`
+	Password string `json:"password"`
+}
+
+type User struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	UserName string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Response struct {
+	Data string `json:"data"`
+}
+
+type Token struct {
+	Token []byte `json:"token"`
+}
+
 
 //初始化Person
 var people []Person
@@ -78,8 +107,139 @@ func main() {
 	router.HandleFunc("/people/{id}",postPerson).Methods("POST")
 	router.HandleFunc("/people/{id}",deletePerson).Methods("DELETE")
 
+	router.HandleFunc("/login",PostLogin).Methods("POST")
+	
+	router.Handle("/resource",
+		negroni.New(
+			negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+
+				token, err := request.ParseFromRequest(r, request.AuthorizationHeaderExtractor,
+					func(token *jwt.Token) (interface{}, error) {
+						return []byte(SecretKey), nil
+					})
+fmt.Println(r.Header.Get("Authorization"))
+				if err == nil {
+					if token.Valid {
+						next(w, r)
+					} else {
+						w.WriteHeader(http.StatusUnauthorized)
+						fmt.Fprint(w, "Token is not valid")
+					}
+				} else {
+					//fmt.Println(err.Error())
+					w.WriteHeader(http.StatusUnauthorized)
+					fmt.Fprint(w, "Unauthorized access to this resource")
+				}
+			}),
+			negroni.Wrap(http.HandlerFunc(ProtectedHandler)),
+		))
+	log.Println("Now listening...")
 	if err :=http.ListenAndServe(":8888",router); err !=nil{
 		log.Fatal(err.Error())
+	}
+}
+
+func PostLogin(w http.ResponseWriter, r *http.Request)  {
+	var user UserCredentials
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err !=nil {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprint(w,"Error in request")
+		return
+	}
+
+	if strings.ToLower(user.UserName) != "admin888" || strings.ToLower(user.Password) != "secret"{
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Println("Error logging in")
+		fmt.Fprint(w,"Invalid credentials")
+		return
+	}
+
+	token :=jwt.New(jwt.SigningMethodHS256)
+	claims := make(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(time.Hour*time.Duration(1)).Unix()
+	claims["iat"] = time.Now().Unix()
+	token.Claims =claims
+
+    tokenString,err := token.SignedString([]byte(SecretKey))
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Error extracting the key")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	response := Token{[]byte(tokenString)}
+	JsonResponse(response,w)
+}
+
+
+func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
+
+	response := Response{"Gained access to protected resource"}
+	JsonResponse(response, w)
+
+}
+
+func JsonResponse(response interface{}, w http.ResponseWriter) {
+
+	jsons,err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsons)
+}
+
+func middlewares(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token, err := request.ParseFromRequest(r, request.AuthorizationHeaderExtractor,
+			func(token *jwt.Token) (interface{}, error) {
+				return []byte(SecretKey), nil
+			})
+
+		if err == nil {
+			if token.Valid {
+				next.ServeHTTP(w, r)
+			} else {
+				w.WriteHeader(http.StatusUnauthorized)
+				fmt.Fprint(w, "Token is not valid")
+			}
+		} else {
+			//fmt.Println(err.Error())
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Unauthorized access to this resource")
+		}
+
+	})
+}
+
+func ValidateTokenMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+
+
+	token, err := request.ParseFromRequest(r, request.AuthorizationHeaderExtractor,
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(SecretKey), nil
+		})
+
+	if err == nil {
+		if token.Valid {
+			next(w, r)
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Token is not valid")
+		}
+	} else {
+		fmt.Println(err.Error())
+		//w.WriteHeader(http.StatusUnauthorized)
+		//fmt.Fprint(w, "Unauthorized access to this resource")
 	}
 
 }
